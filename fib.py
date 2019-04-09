b73ef9e14a09cc1eb110ae27ace6727a5fde472a
Kwonjh1123 <kwonteoat1541@gmail.com>
2019-04-09 23:35:33 +0900

    New file added

--- fib.py
+++ fib.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 a, b = 1, 0
-n = raw input("input a number (n must be a same or bigger than 1)")
+n = int(input("input a number (n must be a same or bigger than 1): "))
 for i in range(n):
   a, b = b, a + b
-print a
+print (b)
